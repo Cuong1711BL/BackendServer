5604becc87fa7e612e6583c222cdccdfdf089f96
CuongHuynh <>
2023-04-09 01:28:14 +0700

    This an update code.

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import cv2
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-# ]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
